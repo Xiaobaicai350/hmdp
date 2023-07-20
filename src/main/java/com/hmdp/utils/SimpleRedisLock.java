c2d9a96a12deb33db2616e3c5e99871f47f34288
胥天昊 <1499487526@qq.com>
2023-07-20 19:00:01 +0800

    解决redis中锁的误删情况，但是仍然存在问题，因为查询和删除不是一个原子性操作

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -14,20 +15,27 @@ public class SimpleRedisLock implements ILock{
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标示
-        long threadId = Thread.currentThread().getId();
-        // 获取锁。这里设置锁的时候设置了超时时间，防止因为服务宕机造成的死锁问题
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
-        //这里只是做了一个简单的拆箱判断，防止空指针问题
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        //通过del删除锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的标示
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断标示是否一致
+        if(threadId.equals(id)) {
+            // 释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
